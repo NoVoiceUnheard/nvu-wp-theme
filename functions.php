9a644b407ee09502ccdfe58625413b6ed6420595
Nathanael Smith <nathanaelnsmith@gmail.com>
2025-02-27 21:05:32 -0800

    add query params to body class, toggle form by param

--- functions.php
+++ functions.php
@@ -5,3 +5,19 @@ function novoiceunheard_enqueue_styles() {
     wp_enqueue_style('novoiceunheard', get_stylesheet_directory_uri() . '/style.css', ['twentytwentyfive']);
 }
 add_action('wp_enqueue_scripts', 'novoiceunheard_enqueue_styles');
+
+// add query params to body class
+function add_query_params_to_body_class($classes) {
+    if (!empty($_GET)) {
+        foreach ($_GET as $key => $value) {
+            $sanitized_key = sanitize_html_class($key);
+            $sanitized_value = sanitize_html_class($value);
+            $classes[] = "query-{$sanitized_key}";
+            if (!empty($sanitized_value)) {
+                $classes[] = "query-{$sanitized_key}-{$sanitized_value}";
+            }
+        }
+    }
+    return $classes;
+}
+add_filter('body_class', 'add_query_params_to_body_class');
